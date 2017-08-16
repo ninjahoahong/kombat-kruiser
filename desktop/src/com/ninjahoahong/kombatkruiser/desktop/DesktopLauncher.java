d286f7fa49a7385cf874c87452e97e42ea7f87dd
Anh Duong <ninjahoahong@users.noreply.github.com>
2017-08-16 17:50:18 +0300

    Setup project still have problem with gretty

---
+++ desktop/src/com/ninjahoahong/kombatkruiser/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ninjahoahong.kombatkruiser.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ninjahoahong.kombatkruiser.KombatKruiser;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KombatKruiser(), config);
+	}
+}
